1385b0ddc8f68700f01b1837a3f70ca7fd4e7e6c
Sven Wille <wille.sven.public@gmail.com>
2017-04-24 07:25:18 +0200

    added solutions to Reader.P01

--- src/Haskell99/Reader/P01.hs
+++ src/Haskell99/Reader/P01.hs
@@ -1,1 +1,27 @@
-fille me 
+module Haskell99.P01
+    (
+    ) where
+
+import Control.Monad.Reader
+
+
+--basic example , crashes on empty lists
+p01_1 :: [a] -> a
+p01_1 ls = runReader readerFunction ls
+  where
+    readerFunction :: Reader [a] a
+    readerFunction = do
+      list <- ask
+      return (last list)
+
+
+--safe version
+p01_2 :: [a] -> Maybe a
+p01_2 ls = runReader readerFunction ls
+  where
+    readerFunction :: Reader [a] (Maybe a)
+    readerFunction = do
+      list <- ask
+      if null list
+        then return Nothing
+        else return (Just $ last list)
