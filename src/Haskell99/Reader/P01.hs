b845eccec1b91d3f15ae6053c2bf5a94879f289a
Sven Wille <wille.sven.public@gmail.com>
2017-04-24 07:39:02 +0200

    added two solutions to reader p01

--- src/Haskell99/Reader/P01.hs
+++ src/Haskell99/Reader/P01.hs
@@ -25,3 +25,18 @@ p01_2 ls = runReader readerFunction ls
       if null list
         then return Nothing
         else return (Just $ last list)
+
+
+--without the syntactic sugar (simple version)
+p01_3 :: [a] -> a
+p01_3 ls = runReader readerFunction ls
+  where
+    readerFunction :: Reader [a] a
+    readerFunction  = ask >>= return . last --or use fmap last ask
+
+
+p01_4 :: [a] -> Maybe a
+p01_4 ls = runReader readerFunction ls
+  where
+    readerFunction :: Reader [a] (Maybe a)
+    readerFunction = ask >>= return . (\list -> if null list then Nothing else Just $ last list)  
